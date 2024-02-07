9cdd4d26d2ec9a5cd036f28baca609ec06151218
v-karthik-kumar <voona.karthik8@gmail.com>
2024-02-07 17:09:37 +0530

    new-dag

---
+++ new_dag.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from datetime import datetime,timedelta
+from airflow.operators.python import PythonOperator
+
+
+def hello():
+    print("helllo!!!!")
+
+with DAG(dag_id="new_dag",
+    start_date=datetime(2024,1,22),
+    schedule_interval="*/5 * * * *",
+    catchup=False) as dag:
+
+    first_task = PythonOperator(
+        task_id = "first_task",
+        python_callable = hello
+    )
+
+first_task
+
